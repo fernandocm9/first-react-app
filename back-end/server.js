4f36f5986df7f95160097ab8dfa45fa2e00b7e8c
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2022-01-28 16:10:31 -0600

    css done for header and footer

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
 dotenv.config({path: '../.env'})
-import express, { response } from 'express'
+import express from 'express'
 import pool from './pg.js'
+import cors from 'cors'
 const app = express()
 const PORT = process.env.PORT || 5050
-
+// app.use(cors())
 app.use(express.json())
 
 
